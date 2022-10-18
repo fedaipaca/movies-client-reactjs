57e08835d713154d937ccc44d232827536fd84c5
Fedai Paca <fedaipaca@gmail.com>
2022-10-18 23:36:36 +0300

    Add counter and search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Counter from "./components/Counter";
 import Hello from "./components/Hello";
+import SearchForm from "./components/Search/SearchForm";
 
 function App() {
   return (
     <div>
       <Hello></Hello>
+      <Counter></Counter>
+      <SearchForm></SearchForm>
     </div>
   );
 }
---
+++ src/components/Search/SearchInput.tsx
@@ -0,0 +1,5 @@
+const SearchInput: React.FC = () => {
+  return <input type="text" placeholder="What do you want to watch?" />;
+};
+
+export default SearchInput;
