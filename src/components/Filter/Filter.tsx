e461e31d8a48f213ea6318389a49d45aba195f9d
Fedai Paca <fedai_paca@epam.com>
2022-11-08 22:47:09 +0300

    refactor: move filter options to component

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,19 +1,28 @@
 import classes from "./Filter.module.css";
+import FilterOption from "./FilterOption";
+
+const filters = [
+  { id: "1", title: "All" },
+  { id: "2", title: "Documentary" },
+  { id: "3", title: "Comedy" },
+  { id: "4", title: "Horror" },
+  { id: "5", title: "Crime" },
+];
 
 const Filter: React.FC = () => {
-  return <nav className={classes.filter}>
-    <ul className={classes["filter__genres"]}>
-      <li><button className={`${classes["filter__genre"]} ${classes["filter__genre--active"]}`}>All</button></li>
-      <li><button className={classes["filter__genre"]}>Documentary</button></li>
-      <li><button className={classes["filter__genre"]}>Comedy</button></li>
-      <li><button className={classes["filter__genre"]}>Horror</button></li>
-      <li><button className={classes["filter__genre"]}>Crime</button></li>
-    </ul>
-    <div className={classes["filter__sort"]}>
-      <span className={classes["filter__sort-tag"]}>SORT BY</span>
-      <button className={classes["filter__sort-btn"]}>Release Date</button>
-    </div>
-  </nav>
+  return (
+    <nav className={classes.filter}>
+      <ul className={classes["filter__genres"]}>
+        {filters?.map((filter) => (
+          <FilterOption id={filter.id} title={filter.title} isActive={filter.title === "All"} />
+        ))}
+      </ul>
+      <div className={classes["filter__sort"]}>
+        <span className={classes["filter__sort-tag"]}>SORT BY</span>
+        <button className={classes["filter__sort-btn"]}>Release Date</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Filter;
