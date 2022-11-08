a6eb9cd326f6f7400216162a8fe8852333f498f9
Fedai Paca <fedai_paca@epam.com>
2022-11-08 23:06:23 +0300

    refactor: add key values

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -2,11 +2,11 @@ import classes from "./Filter.module.css";
 import FilterOption from "./FilterOption";
 
 const filters = [
-  { id: "1", title: "All" },
-  { id: "2", title: "Documentary" },
-  { id: "3", title: "Comedy" },
-  { id: "4", title: "Horror" },
-  { id: "5", title: "Crime" },
+  { id: "a1", title: "All" },
+  { id: "b2", title: "Documentary" },
+  { id: "c3", title: "Comedy" },
+  { id: "d4", title: "Horror" },
+  { id: "e5", title: "Crime" },
 ];
 
 const years = [2022, 2021, 2020, 2019];
@@ -16,7 +16,7 @@ const Filter: React.FC = () => {
     <nav className={classes.filter}>
       <ul className={classes["filter__genres"]}>
         {filters?.map((filter) => (
-          <FilterOption id={filter.id} title={filter.title} isActive={filter.title === "All"} />
+          <FilterOption key={filter.id} id={filter.id} title={filter.title} isActive={filter.title === "All"} />
         ))}
       </ul>
       <div className={classes["filter__sort"]}>
@@ -27,7 +27,7 @@ const Filter: React.FC = () => {
         <select name="years" id="release-date">
           <option value="">RELEASE DATE</option>
           {years.map((year) => (
-            <option value={year}>{year}</option>
+            <option key={year} value={year}>{year}</option>
           ))}
         </select>
       </div>
--- src/components/MovieCardList/MovieCardList.tsx
+++ src/components/MovieCardList/MovieCardList.tsx
@@ -10,7 +10,14 @@ const MovieCardList: React.FC<Movies> = ({ movies }) => {
   return (
     <div className={classes.list}>
       {movies?.map((movie) => (
-        <MovieCard id={movie.id} name={movie.name} genre={movie.genre} year={movie.year} poster={movie.poster} />
+        <MovieCard
+          key={movie.id}
+          id={movie.id}
+          name={movie.name}
+          genre={movie.genre}
+          year={movie.year}
+          poster={movie.poster}
+        />
       ))}
     </div>
   );
